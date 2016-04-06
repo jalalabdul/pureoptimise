0b4322f5e45a6e796db7a0f39112236cbf5f1ee9
jalal <subluq@gmail.com>
2016-04-06 18:08:12 +0700

    padding etc

--- js/main.js
+++ js/main.js
@@ -31,19 +31,6 @@ $(".navbar-nav li a").click(function(event) {
 });
 
     /*====================================
-    Preloader
-    ======================================*/
-	
-  	$(window).load(function() {
-
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
-
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();      
-
-  	}) 
-    /*====================================
     Show Menu on Book
     ======================================*/
     $(window).bind('scroll', function() {

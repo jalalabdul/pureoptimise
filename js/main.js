92030ef71a31c98aeff64faf95775e6e05c8d521
jalal <subluq@gmail.com>
2016-04-06 15:13:14 +0700

    update jquery

--- js/main.js
+++ js/main.js
@@ -20,6 +20,15 @@ function main() {
           }
         }
       });
+    
+    /* ==============================================
+  	Fix navbar collapse
+  	=============================================== */ 
+
+$(".navbar-nav li a").click(function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".bs-example-navbar-collapse-1").collapse('hide');
+});
 
     /*====================================
     Preloader

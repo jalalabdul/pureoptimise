532bee0e97cd4dd1970daaf078e148c9046434fc
jalal <subluq@gmail.com>
2016-04-12 21:05:01 +0700

    revert email form

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,8 +10,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var bizsize = $("select#bizsize").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -24,8 +22,6 @@ $(function() {
                 data: {
                     name: name,
                     email: email,
-                    phone: phone,
-                    bizsize: bizsize,
                     message: message
                 },
                 cache: false,
@@ -35,7 +31,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Thank you for getting in touch. We will reply to you within 2 working days. <br>Kind regards,<br>The Pure Team.</strong>");
+                        .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 

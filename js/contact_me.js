03f53d97ff0d13890c31171b6729366eb2088000
jalal <subluq@gmail.com>
2016-04-12 19:50:06 +0700

    fix email form

--- js/contact_me.js
+++ js/contact_me.js
@@ -25,7 +25,7 @@ $(function() {
                     name: name,
                     email: email,
                     phone: phone,
-                    size: size,
+                    bizsize: bizsize,
                     message: message
                 },
                 cache: false,
@@ -35,7 +35,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Thank you for getting in touch. We will reply to you within 2 working days. <br>Kind regards,<br>The Pure Team. </strong>");
+                        .append("<strong>Thank you for getting in touch. We will reply to you within 2 working days. <br>Kind regards,<br>The Pure Team.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,25 +4,25 @@ if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
    empty($_POST['bizsize']) 	||
-   empty($_POST['message'])	    ||
+   empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
-
+	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
-$business_size = $_POST['bizsize'];
+$bizsize = $_POST['bizsize'];
 $message = $_POST['message'];
-
+	
 // Create the email and send the message
 $to = 'berian@pureoptimisation.co.uk'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nBusiness Size: $business_size\n\nMessage:\n$message";
-$headers = "From: berian@pureoptimisation.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message\n\nPhone:\n$phone\n\nBusiness Size\n$bizsize";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;
+return true;			
 ?>

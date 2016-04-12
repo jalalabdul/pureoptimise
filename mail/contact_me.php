6fcd434999bb3ed23dc99aac447410be558dee20
jalal <subluq@gmail.com>
2016-04-12 19:58:52 +0700

    fix email structure

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,7 +20,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'berian@pureoptimisation.co.uk'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message\n\nPhone:\n$phone\n\nBusiness Size\n$bizsize";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nBusiness Size: $bizsize\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);

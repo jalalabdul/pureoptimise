892e4f897feafd82cb00884e49046d331a8701a6
berianpure <berian@pureoptimisation.co.uk>
2016-04-12 09:44:18 +0100

    Change messaging on contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you for getting in touch. We will reply to you within 2 working days. <br>Kind regards,<br>The Pure Team. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
